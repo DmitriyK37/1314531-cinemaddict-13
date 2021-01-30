428ba42f7aada7c1d9e2fa7ee0fc5f574e8de5d6
DmitriyK37 <qsc1986@mail.ru>
2021-01-30 20:08:26 +0300

    На проверку 1.2

--- src/view/footer-stats.js
+++ src/view/footer-stats.js
@@ -1,17 +1,3 @@
-// import Abstract from "./abstract.js";
-
-// const createQuantityFilm = () => (
-//   `<section class="footer__statistics">
-//     <p>130 291 movies inside</p>
-//   </section>`
-// );
-
-// export default class QuantityFilm extends Abstract {
-//   getTemplate() {
-//     return createQuantityFilm();
-//   }
-// }
-
 import Abstract from "./abstract.js";
 
 const createQuantityFilm = (allFilms) => {
